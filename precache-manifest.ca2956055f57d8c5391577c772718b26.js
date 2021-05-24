45c23387551ee6c1a88e9e50c6e3b3a85a996944
giorgi <giorgiebanoidze90@gmail.com>
2021-05-24 11:55:09 +0300

    created portfolio page

---
+++ precache-manifest.ca2956055f57d8c5391577c772718b26.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb4984f8c17609e86b1e33cc48854aba",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2170f228347c36a9047a",
+    "url": "/portfolio/static/css/main.62d5269a.chunk.css"
+  },
+  {
+    "revision": "d019e5b20f68ce141c7c",
+    "url": "/portfolio/static/js/2.1cd3be4a.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/portfolio/static/js/2.1cd3be4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2170f228347c36a9047a",
+    "url": "/portfolio/static/js/main.d6c82818.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/portfolio/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/portfolio/static/media/html-css-javascript-lg.54fd4234.jpg"
+  },
+  {
+    "revision": "2c7701aae07adecdb0a7e336dff08def",
+    "url": "/portfolio/static/media/html-css-javascript.2c7701aa.jpg"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/portfolio/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/portfolio/static/media/mern-stack.90616a5d.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/portfolio/static/media/react-redux.a8b012a0.jpg"
+  },
+  {
+    "revision": "f370c1e0adb80194dd3ea10e468bc69a",
+    "url": "/portfolio/static/media/react.f370c1e0.png"
+  }
+]);
